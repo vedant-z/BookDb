a24ec1c8c8031c37468787408c83e15b81cd81ce
vedant-z <coolvedant.45@gmail.com>
2024-03-26 21:20:36 +0530

    Initial Setup

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const mongoose = require('mongoose')
+
+require('dotenv').config()
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => {
+    console.log('Connected to MongoDB database')
+})
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+app.use('/', indexRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
