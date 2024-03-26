264e0366386292711f71d11e0ceee7b4eecf61ed
vedant-z <coolvedant.45@gmail.com>
2024-03-26 21:40:10 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 require('dotenv').config()
@@ -20,14 +21,17 @@ db.once('open', () => {
 })
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
